7312e811e9e7ad96b4a1e2c7b744bed680e2817d
zaytsev <zaytsev@cad.ru>
2021-10-18 10:15:41 +0300

    fix

--- MsSqlDatabase.cs
+++ MsSqlDatabase.cs
@@ -33,13 +33,41 @@ namespace TimeWorkTracking
                 sqlConnection.Open();
                 using (var sqlCommand = sqlConnection.CreateCommand())
                 {
-                    //первичные таблицы
-                    //Подразделение
+                    //График работы (таблица для списка) Почасовой/Поминутный
+                    sqlCommand.CommandText = "CREATE TABLE WorkScheme (Id int PRIMARY KEY IDENTITY, Name NVARCHAR(150) NOT NULL UNIQUE)";
+                    sqlCommand.ExecuteNonQuery();
+                    sqlCommand.CommandText = "INSERT INTO WorkScheme(Name) VALUES (N'Почасовой'), (N'Поминутный')";
+                    sqlCommand.ExecuteNonQuery();
+
+                    //Тип даты производственного календаря (таблица для списка) Выходной/Сокращенный
+                    sqlCommand.CommandText = "CREATE TABLE DateType (Id int PRIMARY KEY IDENTITY, Name NVARCHAR(150) NOT NULL UNIQUE)";
+                    sqlCommand.ExecuteNonQuery();
+                    sqlCommand.CommandText = "INSERT INTO DateType(Name) VALUES (N'Выходной'), (N'Сокращенный')";
+                    sqlCommand.ExecuteNonQuery();
+
+                    //Наименование даты производственного календаря (таблица для списка) 
+                    sqlCommand.CommandText = "CREATE TABLE DateName (Id int PRIMARY KEY IDENTITY, Name NVARCHAR(150) NOT NULL UNIQUE, Note NVARCHAR(150))";
+                    sqlCommand.ExecuteNonQuery();
+                    sqlCommand.CommandText = "INSERT INTO DateName(Name, Note) VALUES " +
+                        "(N'Новый год', '1 января'), " +
+                        "(N'Новогодние каникулы', ''), " +
+                        "(N'Рождество Христово', '7 января'), " +
+                        "(N'День защитника Отечества', '23 февраля'), " +
+                        "(N'Международный женский день', '8 марта'), " +
+                        "(N'Праздник весны и труда', '1 мая'), " +
+                        "(N'День Победы', '9 мая'), " +
+                        "(N'День России', '12 июня'), " +
+                        "(N'День народного единства', '4 ноября')";
+                    sqlCommand.ExecuteNonQuery();
+
+                    //Подразделение (таблица для списка)
                     sqlCommand.CommandText = "CREATE TABLE Department (" +
                         "Id int PRIMARY KEY IDENTITY, " +
+                        "ExchangeKey NVARCHAR(20) NOT NULL UNIQUE, " +
                         "Name NVARCHAR(150) NOT NULL UNIQUE " +
                         ")";
                     sqlCommand.ExecuteNonQuery();
+/*
                     //вставляем данные
                     sqlCommand.CommandText = "INSERT INTO Department(Name) VALUES " +
                         "(N'Бухгалтерия'), " +
@@ -53,12 +81,15 @@ namespace TimeWorkTracking
                         "(N'Общее руководство'), " +
                         "(N'Представительство Академия САПР и ГИС')";
                     sqlCommand.ExecuteNonQuery();
-                    //Должность
+ */ 
+                    //Должность (таблица для списка)
                     sqlCommand.CommandText = "CREATE TABLE Post (" +
                         "Id int PRIMARY KEY IDENTITY, " +
+                        "ExchangeKey NVARCHAR(20) NOT NULL UNIQUE, " +
                         "Name NVARCHAR(150) NOT NULL UNIQUE " +
                         ")";
                     sqlCommand.ExecuteNonQuery();
+/*
                     //вставляем данные
                     sqlCommand.CommandText = "INSERT INTO Post(Name) VALUES " +
                         "(N'Ассистент менеджера'), " +
@@ -82,31 +113,80 @@ namespace TimeWorkTracking
                         "(N'Заместитель генерального директора'), " +
                         "(N'и.о. Руководителя')";
                     sqlCommand.ExecuteNonQuery();
-
-                    //График работы
-                    sqlCommand.CommandText = "CREATE TABLE Rate (" +
+*/
+                    //Специальные отметки (самостоятельная таблица)
+                    sqlCommand.CommandText = "CREATE TABLE SpecialMarks (" +
                         "Id int PRIMARY KEY IDENTITY, " +
-                        "Name NVARCHAR(150) NOT NULL UNIQUE " +
+                        "ExchangeKey NVARCHAR(20) NOT NULL UNIQUE, " +
+                        "Name NVARCHAR(150) NOT NULL UNIQUE, " +
+                        "ShortNameCode NVARCHAR(4) NOT NULL, " +
+                        "ShortName NVARCHAR(4) NOT NULL, " +
+                        "Uses bit NOT NULL " +
                         ")";
                     sqlCommand.ExecuteNonQuery();
-                    //вставляем данные
-                    sqlCommand.CommandText = "INSERT INTO Rate(Name) VALUES " +
-                        "(N'Почасовой'), " +
-                        "(N'Поминутный')";
+                    sqlCommand.CommandText = "INSERT INTO SpecialMarks(ExchangeKey, Name, ShortNameCode, ShortName, Uses) VALUES " +
+                        "(N'20141107083705', '-', 'Я', '01', 1), " +
+                        "(N'20141107083706', 'Служебное задание', 'СЗ', '00', 1), " +
+                        "(N'20141107083707', 'Работа из дома', 'РД', '00', 0), " +
+                        "(N'20141107083708', 'Отпуск', 'ОТ', '09', 1), " +
+                        "(N'20141107083709', 'Общественное дело', 'ОД', '00', 1), " +
+                        "(N'20141107083710', 'Личные дела', 'ЛД', '00', 1), " +
+                        "(N'20141107083711', 'Служебная командировка', 'К', '06', 0), " +
+                        "(N'20141107083712', 'Отпуск по беременности и родам', 'Р', '13', 0), " +
+                        "(N'20141107083713', 'Отпуск по уходу за ребенком', 'ОЖ', '15', 0), " +
+                        "(N'20141107083714', 'Отгул', 'ДО', '16', 1), " +
+                        "(N'20141107083715', 'Больничный', 'Б', '19', 1), " +
+                        "(N'20141107083716', 'Прогул', 'ПР', '26', 0), " +
+                        "(N'20210701135120', 'Удаленка', 'УД', '01', 1)";
                     sqlCommand.ExecuteNonQuery();
-                    //смешанные таблицы
-                    //Пользователь
+
+                    //Производственный Календарь (таблица использующая внешние данные)
+                    sqlCommand.CommandText = "CREATE TABLE ProductionCalendar (" +
+                        "Id int PRIMARY KEY IDENTITY, " +
+                        "ExchangeKey NVARCHAR(20) NOT NULL UNIQUE, " +
+                        "OriginalDate Date NOT NULL UNIQUE, " +
+                        "TransferDate Date NOT NULL, " +
+                        "DateTypeId int NOT NULL FOREIGN KEY REFERENCES DateType(Id), " +
+                        "DateNameId int NOT NULL FOREIGN KEY REFERENCES DateName(Id) " +
+                        ")";
+                    sqlCommand.ExecuteNonQuery();
+
+                    //Пользователь (таблица использующая внешние данные)
                     sqlCommand.CommandText = "CREATE TABLE Users (" +
-                        "Id bigint PRIMARY KEY IDENTITY, " +
-                        "UserId NVARCHAR(50) NOT NULL UNIQUE, " +
+                        "Id int PRIMARY KEY IDENTITY, " +
+                        "ExchangeKey NVARCHAR(20) NOT NULL UNIQUE, " +
                         "Name NVARCHAR(150) NOT NULL UNIQUE, " +
                         "DerartmentId int NOT NULL FOREIGN KEY REFERENCES Department(Id), " +
                         "PostId int NOT NULL FOREIGN KEY REFERENCES Post(Id), " +
                         "TimeStart time NOT NULL, " +
                         "TimeStop time NOT NULL, " +
-                        "RateId int NOT NULL FOREIGN KEY REFERENCES Rate(Id), " +
+                        "Lunch bit NOT NULL, " +
+                        "WorkSchemeId int NOT NULL FOREIGN KEY REFERENCES WorkScheme(Id), " +
                         "Uses bit NOT NULL " +
                         ")"; 
+                    sqlCommand.ExecuteNonQuery();
+
+                    //Учет рабочего времени (таблица использующая внешние данные)
+                    sqlCommand.CommandText = "CREATE TABLE PassEvents (" +
+                        "Id bigint PRIMARY KEY IDENTITY, " +
+                        "Author NVARCHAR(150) NOT NULL, " +
+                        "passDate Date NOT NULL UNIQUE, " +
+                        "passUserId int NOT NULL UNIQUE FOREIGN KEY REFERENCES Users(Id), " +
+                        "passTimeStart time NOT NULL, " +
+                        "passTimeStop time NOT NULL, " +
+                        "infoLunchId bit NOT NULL DEFAULT 1, " +
+                        "infoWorkSchemeId int NULL FOREIGN KEY REFERENCES WorkScheme(Id), " +
+                        "timeScheduleFact int NOT NULL  DEFAULT 0, " +
+                        "timeScheduleWithoutLunch int NOT NULL  DEFAULT 0, " +
+                        "timeScheduleLess int NOT NULL DEFAULT 0, " +
+                        "timeScheduleOver int NOT NULL DEFAULT 0, " +
+                        "specmarkNameId int NULL UNIQUE FOREIGN KEY REFERENCES SpecialMarks(Id), " +
+                        "specmarkTimeStart Datetime NULL, " +
+                        "specmarkTimeStop Datetime NULL, " +
+                        "specmarkNote NVARCHAR(1024) NULL, " +
+                        "totalHoursInWork int NOT NULL DEFAULT 0, " +
+                        "totalHoursOutsideWork int NOT NULL DEFAULT 0" +
+                        ")";
                     sqlCommand.ExecuteNonQuery();
                 }
             }
